6cdd19ec4aed9535f14778dc8deaa75d7abb2160
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-03-13 12:20:10 +1100

    h

--- pathfinding.py
+++ pathfinding.py
@@ -18,10 +18,6 @@ screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("A* Pathfinding App")
 font = pygame.font.SysFont(None, 24)
 
-draw_green = True
-
-# Define the button rectangle
-button_rect = pygame.Rect(window_size[0] - 70, 30, 60, 10)
 
 while True:
     for event in pygame.event.get():
@@ -34,15 +30,9 @@ while True:
             # Check for left mouse button click
             if event.button == 1:
                 mouse_pos = pygame.mouse.get_pos()
-                if button_rect.collidepoint(mouse_pos):
-                    draw_green = not draw_green
-                else:
-                    row = mouse_pos[1] // square_size
-                    col = mouse_pos[0] // square_size
-                    if draw_green:
-                        grid[row][col] = green
-                    else:
-                        grid[row][col] = grey
+                row = mouse_pos[1] // square_size
+                col = mouse_pos[0] // square_size
+                grid[row][col] = green
             # Check for right mouse button click
             elif event.button == 3:
                 mouse_pos = pygame.mouse.get_pos()
@@ -50,22 +40,12 @@ while True:
                 col = mouse_pos[0] // square_size
                 grid[row][col] = red
 
-    # Clear the screen
     screen.fill(white)
 
-    # Draw the button
-    pygame.draw.rect(screen, white, button_rect, 2)
-    if draw_green:
-        button_text = font.render("Green", True, green)
-    else:
-        button_text = font.render("Grey", True, grey)
-    screen.blit(button_text, (window_size[0] - 60, 107))
 
-    # Draw the grid
     for row in range(num_rows):
         for col in range(num_cols):
             rect = pygame.Rect(col * square_size, row * square_size, square_size, square_size)
             pygame.draw.rect(screen, grid[row][col], rect)
 
-    # Update the display
     pygame.display.update()
