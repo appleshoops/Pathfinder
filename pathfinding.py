d7d7bc9da6cceb220d6ceb5d39dc6c2cf8187c33
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-03-14 10:08:43 +1100

    e

--- pathfinding.py
+++ pathfinding.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 
 pygame.init()
 
@@ -17,6 +18,16 @@ white = (255, 255, 255)
 grey = (156, 156, 156)
 
 window_size = (600, 600)
+grid = Grid(matrix = matrix)
+
+start = grid.node(0, 0)
+end = grid.node(5, 5)
+
+finder = AStarFinder(heuristic = any)
+
+path = finder.find_path(start, end, grid)
+
+print(path)
 
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("A* Pathfinding App")
@@ -24,4 +35,4 @@ font = pygame.font.SysFont(None, 24)
 
 
 
-    pygame.display.update()
+pygame.display.update()
