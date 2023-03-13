3e1b2a2863405cd609a918263314122766025a30
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-03-14 10:00:37 +1100

    restarting

--- pathfinding.py
+++ pathfinding.py
@@ -1,7 +1,15 @@
 import pygame
 from pygame.locals import *
+from pathfinding.core.grid import Grid
 
 pygame.init()
+
+matrix = [
+   [1, 1, 1, 1, 1],
+   [0, 1, 1, 1, 1],
+   [1, 1, 1, 1, 1],
+   [1, 1, 1, 1, 1],
+   [1, 1, 1, 1, 1]]
 
 green = (163, 247, 180)
 red = (250, 145, 152)
@@ -9,43 +17,11 @@ white = (255, 255, 255)
 grey = (156, 156, 156)
 
 window_size = (600, 600)
-square_size = 30
-num_rows = 20
-num_cols = 20
-grid = [[white for j in range(num_cols)] for i in range(num_rows)]
 
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("A* Pathfinding App")
 font = pygame.font.SysFont(None, 24)
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-
-        # Check for mouse button events
-        if event.type == MOUSEBUTTONDOWN:
-            # Check for left mouse button click
-            if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                row = mouse_pos[1] // square_size
-                col = mouse_pos[0] // square_size
-                grid[row][col] = green
-            # Check for right mouse button click
-            elif event.button == 3:
-                mouse_pos = pygame.mouse.get_pos()
-                row = mouse_pos[1] // square_size
-                col = mouse_pos[0] // square_size
-                grid[row][col] = red
-
-    screen.fill(white)
-
-    for row in range(num_rows):
-        for col in range(num_cols):
-            rect = pygame.Rect(col * square_size, row *
-                               square_size, square_size, square_size)
-            pygame.draw.rect(screen, grid[row][col], rect)
 
     pygame.display.update()
