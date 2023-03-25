28987c2ad56e61835d14c1a4ebc7e62a3a647cf5
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-03-25 15:07:10 +1100

    bbbbb

--- pathfinding.py
+++ pathfinding.py
@@ -18,6 +18,7 @@ num_rows = 20
 num_cols = 20
 grid = [[(white) for j in range(num_cols)]for i in range(num_rows)]
 
+
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("A* Pathfinding App")
 font = pygame.font.SysFont(None, 24)
@@ -26,19 +27,18 @@ draw_green = True
 
 while True:
 
-    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
 
         pressed = pygame.key.get_pressed()
-        if pressed [pygame.K_LSHIFT]:
-         mouse_pos = pygame.mouse.get_pos()
-         row = mouse_pos[1] // square_size
-         col = mouse_pos[0] // square_size
-         grid[row][col] = grey
-        
-        if pressed [pygame.K_z]:
+        if pressed[pygame.K_LSHIFT]:
+            mouse_pos = pygame.mouse.get_pos()
+            row = mouse_pos[1] // square_size
+            col = mouse_pos[0] // square_size
+            grid[row][col] = grey
+
+        if pressed[pygame.K_z]:
             mouse_pos = pygame.mouse.get_pos()
             row = mouse_pos[1] // square_size
             col = mouse_pos[0] // square_size
@@ -48,7 +48,7 @@ while True:
                     if i != row or j != col:
                         if grid[i][j] == green:
                             grid[i][j] = white
-        if pressed [pygame.K_x]:
+        if pressed[pygame.K_x]:
             mouse_pos = pygame.mouse.get_pos()
             row = mouse_pos[1] // square_size
             col = mouse_pos[0] // square_size
@@ -58,19 +58,18 @@ while True:
                     if i != row or j != col:
                         if grid[i][j] == red:
                             grid[i][j] = white
-                            
-        if pressed [pygame.K_c]:
-           mouse_pos = pygame.mouse.get_pos()
-           row = mouse_pos[1] // square_size
-           col = mouse_pos[0] // square_size
-           grid[row][col] = white
-           for i in range(num_rows):
-               for j in range(num_cols):
-                   if i != row or j != col:
-                       if grid[i][j] == white:
-                           grid[i][j] = white
-           
-                            
+
+        if pressed[pygame.K_c]:
+            mouse_pos = pygame.mouse.get_pos()
+            row = mouse_pos[1] // square_size
+            col = mouse_pos[0] // square_size
+            grid[row][col] = white
+            for i in range(num_rows):
+                for j in range(num_cols):
+                    if i != row or j != col:
+                        if grid[i][j] == white:
+                            grid[i][j] = white
+
     screen.fill(white)
 
     for row in range(num_rows):
