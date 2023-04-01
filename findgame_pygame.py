a7de8a91b20d060cc3040e2ebb061ed1eab28b92
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-04-01 16:08:03 +1100

    making dijsktra

--- findgame_pygame.py
+++ findgame_pygame.py
@@ -1,4 +1,4 @@
-from tkinter import messagebox as tk
+from tkinter import messagebox
 import pygame
 import sys
 
@@ -11,7 +11,14 @@ rows = 30
 square_width = width // columns
 square_height = height // rows
 
+green = (163, 247, 180)
+red = (250, 145, 152)
+white = (255, 255, 255)
+grey = (156, 156, 156)
+blue = (148, 171, 247)
+
 grid = []
+queue = []
 
 class square:
     def __init__(self, i, j):
@@ -20,8 +27,16 @@ class square:
         self.start = False
         self.wall = False
         self.end = False
+        self.queued = False
+        self.visited = False
+        self.neighbors = []
     def draw(self, win, colour):
         pygame.draw.rect(win, colour, (self.x * square_width, self.y * square_height, square_width - 2, square_height -2))
+    def add_neighbors(self):
+        if self.x > 0:
+            self.neighbors.append(grid[self.x - 1][self.y])
+        if self.x < columns - 1:
+            self.neighbors.append(grid[self.x + 1][self.y])
 
 for i in range(columns):
     arr = []
@@ -29,8 +44,14 @@ for i in range(columns):
         arr.append(square(i, j))
     grid.append(arr)
 
+for i in range(columns):
+    for j in range(rows):
+        grid[i][j].add_neighbors()
+
 start_square = grid[0][0]
 start_square.start = True
+start_square.visited = True
+queue.append(start_square)
 
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pathfinding App")
@@ -38,6 +59,8 @@ pygame.display.set_caption("Pathfinding App")
 def main():
     begin_search = False
     target_square_set = False
+    searching = True
+    target_square = None
 
     while True:
         for event in pygame.event.get():
@@ -59,6 +82,23 @@ def main():
                     end_square_set = True
             if event.type == pygame.KEYDOWN and target_square_set:
                 begin_search = True
+        if begin_search:
+            if len(queue) and searching:
+                current_square = queue.pop(0)
+                current_square.visited = True
+                if current_square == target_square:
+                    searching = False
+                    print("Found target square")
+                else:
+                    for neighbor in current_square.neighbors:
+                        if not neighbor.queued and not neighbor.wall:
+                            neighbor.queued = True
+                            queue.append(neighbor)
+            else:
+                if searching:
+                    messagebox().wm_withdraw() # type: ignore
+                    messagebox.showinfo("No Solution", "No path found")
+                    searching = False
 
         window.fill('grey22')
 
@@ -67,7 +107,11 @@ def main():
                 square = grid[i][j]
                 square.draw(window, "gray8")
                 if square.start:
-                    square.draw(window, "green")
+                    square.draw(window, green)
+                if square.wall:
+                    square.draw(window, red)
+                if square.end:
+                    square.draw(window, blue)
 
         pygame.display.flip()
 
