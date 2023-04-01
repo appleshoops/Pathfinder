cceb083caf07e41cb4d12c93b95d936a10d56e87
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-04-01 14:48:47 +1100

    Added Grid Lines

--- findgame_pygame.py
+++ findgame_pygame.py
@@ -1,9 +1,30 @@
-from tkinter import messagebox, Tk
+from tkinter import messagebox as tk
 import pygame
 import sys
 
-width = 500
-height = 500
+width = 600
+height = 600
+
+columns = 30
+rows = 30
+
+square_width = width // columns
+square_height = height // rows
+
+grid = []
+
+class square:
+    def __init__(self, i, j):
+        self.x = i
+        self.y = j
+    def draw(self, win, colour):
+        pygame.draw.rect(win, colour, (self.x * square_width, self.y * square_height, square_width - 2, square_height -2))
+
+for i in range(columns):
+    arr = []
+    for j in range(rows):
+        arr.append(square(i, j))
+    grid.append(arr)
 
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pathfinding App")
@@ -16,6 +37,11 @@ def main():
                 sys.exit()
 
         window.fill('grey22')
+
+        for i in range(columns):
+            for j in range(rows):
+                square = grid[i][j]
+                square.draw(window, "gray8")
 
         pygame.display.flip()
 
