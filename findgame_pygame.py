8b4dbeb73d381c05efa3cf50644d90fe5028c70b
Ethan Eswaran <ethan.eswaran@icloud.com>
2023-04-01 15:22:04 +1100

    added square states

--- findgame_pygame.py
+++ findgame_pygame.py
@@ -17,6 +17,9 @@ class square:
     def __init__(self, i, j):
         self.x = i
         self.y = j
+        self.start = False
+        self.wall = False
+        self.end = False
     def draw(self, win, colour):
         pygame.draw.rect(win, colour, (self.x * square_width, self.y * square_height, square_width - 2, square_height -2))
 
@@ -26,15 +29,36 @@ for i in range(columns):
         arr.append(square(i, j))
     grid.append(arr)
 
+start_square = grid[0][0]
+start_square.start = True
+
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pathfinding App")
 
 def main():
+    begin_search = False
+    target_square_set = False
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.MOUSEMOTION:
+                x = pygame.mouse.get_pos()[0]
+                y = pygame.mouse.get_pos()[1]
+                if event.buttons[0]:
+                    i = x // square_width
+                    j = y // square_height
+                    grid[i][j].wall = True
+                if event.buttons[2] and not target_square_set:
+                    i = x // square_width
+                    j = y // square_height
+                    end_square = grid[i][j]
+                    end_square.end = True
+                    end_square_set = True
+            if event.type == pygame.KEYDOWN and target_square_set:
+                begin_search = True
 
         window.fill('grey22')
 
@@ -42,6 +66,8 @@ def main():
             for j in range(rows):
                 square = grid[i][j]
                 square.draw(window, "gray8")
+                if square.start:
+                    square.draw(window, "green")
 
         pygame.display.flip()
 
